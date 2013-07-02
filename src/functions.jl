64c0f0ed95fea1af76e68b326870418d2bee7455
Xiao He <praguewatermelon@gmail.com>
2013-07-01 21:14:28 -0700

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -82,7 +82,7 @@ function trimci{S <: Real}(x::Vector{S}; tr::Real=0.2, alpha::Real=0.05, nullval
     return output
 end
 #trimci{S <: Real}(x::DataVector{S}; tr=0.2, alpha=0.05, nullvalue=0, method=true)=
-    trimci(removeNA(x), tr=tr, alpha=alpha, nullvalue=nullvalue, method=method)
+ #   trimci(removeNA(x), tr=tr, alpha=alpha, nullvalue=nullvalue, method=method)
 
 
 #Stein's method
